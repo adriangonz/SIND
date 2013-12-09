e70282601351062d7748efc6650ca8aba0005fb3
Adrian Gonzalez <kaseyo23@gmail.com>
2013-12-09 18:13:22 +0100

    Cambios en el cron

--- webapp/cron.py
+++ webapp/cron.py
@@ -3,7 +3,7 @@ from models import ModbusData
 
 
 class ReloadInfo(CronJobBase):
-    RUN_EVERY_MINS = 1
+    RUN_EVERY_MINS = 2
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'webapp.reload_info'    # a unique code
